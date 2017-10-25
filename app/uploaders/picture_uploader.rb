db0df056508275530d70ee97fab98694725874ac
Nick Hunter <hunter.497@gmail.com>
2017-10-24 22:04:27 -0400

    Finishing up the microposts.

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,5 +1,12 @@
 class PictureUploader < CarrierWave::Uploader::Base
-  storage :file
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+
+  if Rails.env.production?
+  	storage :fog
+  else
+  	storage :file
+  end
   
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
