f64b78f8dde3fe7b13c1ce1566153cec92128c03
Nick Hunter <hunter.497@gmail.com>
2017-10-29 21:19:39 -0400

    Starting the blog by creating the post model and changing some of the about text to reflect the idea of the site.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024011248) do
+ActiveRecord::Schema.define(version: 20171030000641) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20171024011248) do
     t.string "picture"
     t.index ["user_id", nil], name: "index_microposts_on_user_id_and_created_ats"
     t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
